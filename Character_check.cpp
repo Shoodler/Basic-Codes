4844671684ecc6ea7ecab90f9a764456f9b8285e
Rohan Upadhye <121246963+sienstalker@users.noreply.github.com>
2022-12-27 13:04:43 +0530

    Add files via upload

---
+++ Character_check.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main()
+{
+    char ch;
+    system("cls");
+    cout<<"Enter a Character"<<endl;
+    cin>>ch;
+    if(ch>='A' && ch<='Z')
+        cout<<"Uppercase"<<endl;
+    else if(ch>='a' && ch<='z')
+        cout<<"Lowercase"<<endl;
+    else if(ch>='1' && ch<='9')
+        cout<<"Digit"<<endl;
+    else
+        cout<<"Special Character"<<endl;
+    return 0;
+}
