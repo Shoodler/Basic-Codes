520766395fd5de508680a30164509633e70d5ffc
Rohan Upadhye <121246963+sienstalker@users.noreply.github.com>
2023-01-07 12:44:38 +0530

    Update Quadratic_Inequalities.cpp

--- Quadratic_Inequalities.cpp
+++ Quadratic_Inequalities.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <conio.h>
 #include <cmath>
 using namespace std;
 int main()
@@ -38,6 +37,5 @@ int main()
         default: cout<<"Invalid Input"<<endl;
 
     }
-    getch();
     return 0;
 }
