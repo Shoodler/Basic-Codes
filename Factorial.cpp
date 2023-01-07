3f8ac2b12b5be7116ecc7af14a80511934ca60f4
Rohan Upadhye <121246963+sienstalker@users.noreply.github.com>
2023-01-07 12:40:54 +0530

    Add files via upload

---
+++ Factorial.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    system("cls");
+    int f,a,l;
+    cout<<"Enter a Number for factorial calculation"<<endl;
+    cin>>a;
+    f=1;
+    l=1;
+    while(l<=a)
+    {
+        f=f*l;
+        l++;
+    }
+    cout<<"Answer = "<<f<<endl;
+    return 0;
+}
