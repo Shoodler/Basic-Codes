8a301b91c9205d4e6bacf5b427d2b1d4eadf085d
Rohan Upadhye <121246963+sienstalker@users.noreply.github.com>
2023-01-07 12:34:53 +0530

    Update Factorial.cpp

--- Factorial.cpp
+++ Factorial.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <conio.h>
 using namespace std;
 int main()
 {
@@ -17,6 +16,5 @@ int main()
         l++;
     }
     cout<<"Factorial ="<<c<<endl;
-    getch();
     return 0;
 }
