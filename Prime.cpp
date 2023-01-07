13be46b7308698f61c96e474036eb27de5ac5df2
Rohan Upadhye <121246963+sienstalker@users.noreply.github.com>
2023-01-07 12:44:06 +0530

    Update Prime.cpp

--- Prime.cpp
+++ Prime.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <conio.h>
 using namespace std;
 int main()
 {
@@ -23,6 +22,5 @@ int main()
         cout<<"Number is Composite"<<endl;
     else if (t==0)
         cout<<"Number is Prime"<<endl;
-    getch();
     return 0;
 }
